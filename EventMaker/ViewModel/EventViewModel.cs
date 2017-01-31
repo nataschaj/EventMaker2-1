d13bcdabe57060eafc1378e3c2cc9b9f379426f8
natsachaj <natascha_jakobsen@hotmail.com>
2017-01-31 10:31:25 +0100

    update

--- EventMaker/ViewModel/EventViewModel.cs
+++ EventMaker/ViewModel/EventViewModel.cs
@@ -11,5 +11,13 @@ namespace EventMaker.ViewModel
     public class EventViewModel
     {
         public Model.EventCatalogSingleton SingletonRef { get; set; }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Place { get; set; }
+        public DateTimeOffset Date { get; set; }
+        public TimeSpan Time { get; set; }
+
+
     }
 }
